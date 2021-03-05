7e4ee13bf2844c371094032d06716e1220466628
mkihr-ojisan <52490041+mkihr-ojisan@users.noreply.github.com>
2021-03-05 13:42:18 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true,
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module',
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4, 
+            {'SwitchCase': 1},
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+};
---
+++ README.md
@@ -0,0 +1,14 @@
+# modpack-downloader
+
+Curseforge modpack downloader for Linux
+
+## Build
+```console
+$ npx tsc
+```
+
+## Usage
+
+```console
+$ node index.js 'curseforge://install?addonId=283861&fileId=3133852' ~/.minecraft/ftb_revelations
+```
